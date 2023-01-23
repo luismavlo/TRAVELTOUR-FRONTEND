ee2d4dee48ecdd0ac9ef82399d11ed6562edda94
Luis Miguel <lmavendano1597@gmail.com>
2023-01-23 14:58:17 -0500

    estilos agregados

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Tours } from './Tours'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Tours } from './Tours';
+import './css/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Tours />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
